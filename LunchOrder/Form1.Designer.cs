00dd2ea77339df66fdcbd916377c6d63ff63c94e
ossebi-gc <79544566+ossebi-gc@users.noreply.github.com>
2023-04-22 19:34:35 -0400

    Add project files.

---
+++ LunchOrder/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace LunchOrder
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbxMainCourse = new System.Windows.Forms.GroupBox();
+            this.radioSalad = new System.Windows.Forms.RadioButton();
+            this.radioPizza = new System.Windows.Forms.RadioButton();
+            this.radioHamburger = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtOrderTotal = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSalesTax = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSubtotal = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnPlaceOrder = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkFrenchfries = new System.Windows.Forms.CheckBox();
+            this.checkKetchupMustardMayo = new System.Windows.Forms.CheckBox();
+            this.checkLettuceTomatoOnion = new System.Windows.Forms.CheckBox();
+            this.gbxMainCourse.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbxMainCourse
+            // 
+            this.gbxMainCourse.Controls.Add(this.radioSalad);
+            this.gbxMainCourse.Controls.Add(this.radioPizza);
+            this.gbxMainCourse.Controls.Add(this.radioHamburger);
+            this.gbxMainCourse.Location = new System.Drawing.Point(49, 28);
+            this.gbxMainCourse.Name = "gbxMainCourse";
+            this.gbxMainCourse.Size = new System.Drawing.Size(227, 163);
+            this.gbxMainCourse.TabIndex = 0;
+            this.gbxMainCourse.TabStop = false;
+            this.gbxMainCourse.Text = "Main Course";
+            // 
+            // radioSalad
+            // 
+            this.radioSalad.AutoSize = true;
+            this.radioSalad.Location = new System.Drawing.Point(20, 112);
+            this.radioSalad.Name = "radioSalad";
+            this.radioSalad.Size = new System.Drawing.Size(141, 29);
+            this.radioSalad.TabIndex = 2;
+            this.radioSalad.Text = "Salad - $4.95";
+            this.radioSalad.UseVisualStyleBackColor = true;
+            this.radioSalad.CheckedChanged += new System.EventHandler(this.radioSalad_CheckedChanged_1);
+            // 
+            // radioPizza
+            // 
+            this.radioPizza.AutoSize = true;
+            this.radioPizza.Location = new System.Drawing.Point(20, 77);
+            this.radioPizza.Name = "radioPizza";
+            this.radioPizza.Size = new System.Drawing.Size(137, 29);
+            this.radioPizza.TabIndex = 1;
+            this.radioPizza.Text = "Pizza - $5.95";
+            this.radioPizza.UseVisualStyleBackColor = true;
+            this.radioPizza.CheckedChanged += new System.EventHandler(this.radioPizza_CheckedChanged_1);
+            // 
+            // radioHamburger
+            // 
+            this.radioHamburger.AutoSize = true;
+            this.radioHamburger.Checked = true;
+            this.radioHamburger.Location = new System.Drawing.Point(20, 42);
+            this.radioHamburger.Name = "radioHamburger";
+            this.radioHamburger.Size = new System.Drawing.Size(189, 29);
+            this.radioHamburger.TabIndex = 0;
+            this.radioHamburger.TabStop = true;
+            this.radioHamburger.Text = "Hamburger - $6.95";
+            this.radioHamburger.UseVisualStyleBackColor = true;
+            this.radioHamburger.CheckedChanged += new System.EventHandler(this.radioHamburger_CheckedChanged_1);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtOrderTotal);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txtSalesTax);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.txtSubtotal);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(49, 222);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(371, 192);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Order Total";
+            // 
+            // txtOrderTotal
+            // 
+            this.txtOrderTotal.Location = new System.Drawing.Point(160, 127);
+            this.txtOrderTotal.Name = "txtOrderTotal";
+            this.txtOrderTotal.ReadOnly = true;
+            this.txtOrderTotal.Size = new System.Drawing.Size(150, 31);
+            this.txtOrderTotal.TabIndex = 5;
+            this.txtOrderTotal.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Order Total:";
+            // 
+            // txtSalesTax
+            // 
+            this.txtSalesTax.Location = new System.Drawing.Point(160, 83);
+            this.txtSalesTax.Name = "txtSalesTax";
+            this.txtSalesTax.ReadOnly = true;
+            this.txtSalesTax.Size = new System.Drawing.Size(150, 31);
+            this.txtSalesTax.TabIndex = 3;
+            this.txtSalesTax.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tax (7.75%):";
+            // 
+            // txtSubtotal
+            // 
+            this.txtSubtotal.Location = new System.Drawing.Point(160, 38);
+            this.txtSubtotal.Name = "txtSubtotal";
+            this.txtSubtotal.ReadOnly = true;
+            this.txtSubtotal.Size = new System.Drawing.Size(150, 31);
+            this.txtSubtotal.TabIndex = 1;
+            this.txtSubtotal.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Subtotal:";
+            // 
+            // btnPlaceOrder
+            // 
+            this.btnPlaceOrder.Location = new System.Drawing.Point(457, 238);
+            this.btnPlaceOrder.Name = "btnPlaceOrder";
+            this.btnPlaceOrder.Size = new System.Drawing.Size(111, 42);
+            this.btnPlaceOrder.TabIndex = 2;
+            this.btnPlaceOrder.Text = "Place &Order";
+            this.btnPlaceOrder.UseVisualStyleBackColor = true;
+            this.btnPlaceOrder.Click += new System.EventHandler(this.btnPlaceOrder_Click_1);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(457, 370);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(111, 42);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkFrenchfries);
+            this.groupBox1.Controls.Add(this.checkKetchupMustardMayo);
+            this.groupBox1.Controls.Add(this.checkLettuceTomatoOnion);
+            this.groupBox1.Location = new System.Drawing.Point(324, 44);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(262, 147);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add-on items (.75/each)";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // checkFrenchfries
+            // 
+            this.checkFrenchfries.AutoSize = true;
+            this.checkFrenchfries.Location = new System.Drawing.Point(25, 112);
+            this.checkFrenchfries.Name = "checkFrenchfries";
+            this.checkFrenchfries.Size = new System.Drawing.Size(131, 29);
+            this.checkFrenchfries.TabIndex = 2;
+            this.checkFrenchfries.Text = "French Fries";
+            this.checkFrenchfries.UseVisualStyleBackColor = true;
+            this.checkFrenchfries.CheckedChanged += new System.EventHandler(this.checkFrenchfries_CheckedChanged);
+            // 
+            // checkKetchupMustardMayo
+            // 
+            this.checkKetchupMustardMayo.AutoSize = true;
+            this.checkKetchupMustardMayo.Location = new System.Drawing.Point(25, 77);
+            this.checkKetchupMustardMayo.Name = "checkKetchupMustardMayo";
+            this.checkKetchupMustardMayo.Size = new System.Drawing.Size(231, 29);
+            this.checkKetchupMustardMayo.TabIndex = 1;
+            this.checkKetchupMustardMayo.Text = "Ketchup, Mustard, Mayo";
+            this.checkKetchupMustardMayo.ThreeState = true;
+            this.checkKetchupMustardMayo.UseVisualStyleBackColor = true;
+            this.checkKetchupMustardMayo.CheckedChanged += new System.EventHandler(this.checkKetchupMustardMayo_CheckedChanged);
+            // 
+            // checkLettuceTomatoOnion
+            // 
+            this.checkLettuceTomatoOnion.AutoSize = true;
+            this.checkLettuceTomatoOnion.Location = new System.Drawing.Point(25, 42);
+            this.checkLettuceTomatoOnion.Name = "checkLettuceTomatoOnion";
+            this.checkLettuceTomatoOnion.Size = new System.Drawing.Size(221, 29);
+            this.checkLettuceTomatoOnion.TabIndex = 0;
+            this.checkLettuceTomatoOnion.Text = "Lettuce, Tomato, Onion";
+            this.checkLettuceTomatoOnion.UseVisualStyleBackColor = true;
+            this.checkLettuceTomatoOnion.CheckedChanged += new System.EventHandler(this.checkLettuceTomatoOnion_CheckedChanged_1);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnPlaceOrder;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(619, 457);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnPlaceOrder);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.gbxMainCourse);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Guy-Leroc Ossebi Lunch Order";
+            this.gbxMainCourse.ResumeLayout(false);
+            this.gbxMainCourse.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxMainCourse;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtOrderTotal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSalesTax;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSubtotal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnPlaceOrder;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.RadioButton radioSalad;
+        private System.Windows.Forms.RadioButton radioPizza;
+        private System.Windows.Forms.RadioButton radioHamburger;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkFrenchfries;
+        private System.Windows.Forms.CheckBox checkKetchupMustardMayo;
+        private System.Windows.Forms.CheckBox checkLettuceTomatoOnion;
+    }
+}
+
---
+++ LunchOrder/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LunchOrder
+{
+    public partial class Form1 : Form
+    {
+        const double Tax = 7.75d;
+        const double Hamburger = 6.95d;
+        const double pizza = 5.95d;
+        const double Salad = 4.95d;
+
+        double AddOn = 0.0d; //To hold add on sum
+
+
+        double AddOnPrice = 0.0d; //To hold Add On Price
+        double Order_Total = 0.0d; //To Hold Order Total
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+        private void btnPlaceOrder_Click_1(object sender, EventArgs e)
+        {
+
+
+            if (radioHamburger.Checked)
+                Order_Total += Hamburger; // similar to Order_Total= Order_Total + Hamburger
+            if (radioPizza.Checked)
+                Order_Total += pizza;
+            if (radioSalad.Checked)
+                Order_Total += Salad;
+
+            Order_Total += AddOn;
+
+            txtSubtotal.Text = "$" + String.Format("{0:0.00}", Order_Total);
+
+            double tax = (Order_Total * 7.75d) / 100;
+
+            txtSalesTax.Text = "$" + String.Format("{0:0.00}", tax);
+
+            txtOrderTotal.Text = "$" + String.Format("{0:0.00}", (Order_Total + tax));
+
+
+        }
+        // Guy-leroc Ossebi
+
+        private void checkLettuceTomatoOnion_CheckedChanged_1(object sender, EventArgs e)
+        {
+            if (checkLettuceTomatoOnion.Checked)
+                AddOn += AddOnPrice;
+            else
+                AddOn -= AddOnPrice;
+
+            ClearTotals();
+
+        }
+        // Guy-leroc Ossebi
+        private void checkKetchupMustardMayo_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkKetchupMustardMayo.Checked)
+                AddOn += AddOnPrice;
+            else
+                AddOn -= AddOnPrice;
+        }
+        // Guy-leroc Ossebi
+        private void checkFrenchfries_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkFrenchfries.Checked)
+                AddOn += AddOnPrice;
+            else
+                AddOn -= AddOnPrice;
+        }
+        private void ClearTotals()
+        {
+            txtOrderTotal.Text = string.Empty;
+            txtSubtotal.Text = string.Empty;
+            txtSalesTax.Text = string.Empty;
+
+            Order_Total = 0.0d;
+
+
+
+        }
+        
+        private void ClearAddOns()
+        {
+            AddOn = 0.0d;
+            checkFrenchfries.Checked = false;
+            checkKetchupMustardMayo.Checked = false;
+            checkLettuceTomatoOnion.Checked = false;
+        }
+        // Guy-leroc Ossebi
+        private void radioHamburger_CheckedChanged_1(object sender, EventArgs e)
+        {
+            ClearTotals();
+            ClearAddOns();
+            if (radioHamburger.Checked)
+            {
+                groupBox1.Text = "Add-on items ($.75/each)";
+                checkLettuceTomatoOnion.Text = "Lettuce, tomato and onions";
+                checkKetchupMustardMayo.Text = "Ketchup, mustard and mayo";
+                checkFrenchfries.Text = "French Fries";
+                AddOnPrice = 0.75d;
+            }
+
+        }
+        // Guy-leroc Ossebi
+        private void radioPizza_CheckedChanged_1(object sender, EventArgs e)
+        {
+            ClearTotals();
+            ClearAddOns();
+
+            if (radioPizza.Checked)
+            {
+                groupBox1.Text = "Add-on items ($.50/each)";
+
+                checkLettuceTomatoOnion.Text = "Pepperoni";
+                checkKetchupMustardMayo.Text = "Sausage";
+                checkFrenchfries.Text = "Olives";
+                AddOnPrice = 0.50d;
+            }
+        }
+        // Guy-leroc Ossebi
+        private void radioSalad_CheckedChanged_1(object sender, EventArgs e)
+        {
+
+            ClearTotals();
+            ClearAddOns();
+
+            if (radioSalad.Checked)
+            {
+                groupBox1.Text = "Add-on items ($.25/each)";
+                checkLettuceTomatoOnion.Text = "Croutons";
+                checkKetchupMustardMayo.Text = "Bacon bits";
+                checkFrenchfries.Text = "Bread sticks ";
+                AddOnPrice = 0.25d;
+            }
+        }
+    }
+}
